bef5a3ba001ea5f155d9b18ff9537fddcb4fdd17
leggetter <phil@leggetter.co.uk>
2016-08-11 23:17:58 +0100

    Allow pre-provisioned numbers to be in config/.env

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ var config = {
   NEXMO_API_KEY: process.env['NEXMO_API_KEY'],
   NEXMO_API_SECRET: process.env['NEXMO_API_SECRET'],
   NEXMO_DEBUG: process.env['NEXMO_DEBUG'],
-  NEXMO_APP_ID: process.env['NEXMO_APP_ID']
+  NEXMO_APP_ID: process.env['NEXMO_APP_ID'],
+  PROVISIONED_NUMBERS: process.env['PROVISIONED_NUMBERS']? JSON.parse(process.env['PROVISIONED_NUMBERS']) : []
 };
 
 module.exports = config;
